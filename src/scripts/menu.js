51921a78c7e6bf9fe0e8b277bd43e036625a1c32
Ilya <verron228@mail.ru>
2021-02-26 02:01:14 +0300

    v3

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -12,7 +12,7 @@ wow = new WOW(
     {
     boxClass:     'wow',      // default
     animateClass: 'animated', // default
-    offset:       200,          // default
+    offset:       300,          // default
     mobile:       true,       // default
     live:         true        // default
   }
