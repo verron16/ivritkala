451fc3c4907a7693ae0dd18a3c10561582cd4203
Ilya <verron228@mail.ru>
2021-02-26 17:08:59 +0300

    v3

---
+++ src/scripts/slider.js
@@ -0,0 +1,28 @@
+let swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 15,
+    slidesPerGroup: 2,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            slidesPerGroup: 1
+        },
+        560: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            slidesPerGroup: 1
+        },
+        1180: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+            slidesPerGroup: 2
+        }
+    }
+  });
